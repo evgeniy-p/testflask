c44207896979065c05b059dfdce17971b42e43ae
evgeniy-p <mariwield@gmail.com>
2018-02-12 13:44:45 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 from testapp import app
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
--- testapp/routes.py
+++ testapp/routes.py
@@ -4,6 +4,3 @@ from testapp import app
 @app.route('/index')
 def index():
     return "Hello, World!"
-
-if __name__ == "__main__":
-    application.run(host='0.0.0.0')
