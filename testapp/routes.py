77c934e7883901de5c061412a196b9539a02c3e1
evgeniy-p <mariwield@gmail.com>
2018-02-13 13:33:34 +0300

    update

--- testapp/routes.py
+++ testapp/routes.py
@@ -2,8 +2,9 @@ from flask import render_template
 from testapp import app
 
 
+bdposts = [1, 2, 3, 4, 5, 6]
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('Test.html')
+    return render_template('Test.html', bdposts=bdposts)
