c57deab0de3b5316c92fe34405b58ddcb2d198b1
evgeniy-p <mariwield@gmail.com>
2018-02-13 11:16:42 +0300

    update

--- testapp/routes.py
+++ testapp/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from testapp import app
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='test', username='Человек')
