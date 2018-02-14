230eda1199d038a739fb7009ade26185b78e8fa5
evgeniy-p <mariwield@gmail.com>
2018-02-14 14:06:09 +0300

    update

--- testapp/routes.py
+++ testapp/routes.py
@@ -1,9 +1,29 @@
-from flask import render_template
+from flask import *
 from testapp import app
 
+from_bd = [1, 2]
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='test', username='Человек')
+def main():
+        return redirect(url_for('start'))
+
+
+@app.route('/start')
+def start():
+        return render_template('Test.html', from_bd="", status="", code_client="")
+
+
+@app.route('/post', methods=['POST'])
+def do_entry():
+        try:
+                if not request.form['client_code']:
+                        return render_template('Test.html', res='Введите код клиента!!!')
+                if request.form.get('text_sumb_form'):
+                        return render_template('Test.html', res=request.form['text_sumb_form'] + ' Клиент ' +
+                                                                request.form['client_code'], code_client=request.form['client_code'])
+                if request.form.get('searchbutt'):
+                        return render_template('Test.html', res=request.form['searchbutt']+' Клиент ' +
+                                                                request.form['client_code'], code_client=request.form['client_code'])
+        except BaseException as e:
+                return render_template('Test.html', res=e)
