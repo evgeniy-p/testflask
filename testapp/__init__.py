4d229044f097c6d8d10aca885f74dc575bf1a810
evgeniy-p <mariwield@gmail.com>
2018-02-15 13:56:56 +0300

    update

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+import pymysql
+import conf
 
 app = Flask(__name__)
+try:
+    mysql = pymysql.connect(host='localhost',
+                            user=conf.USR,
+                            password=conf.PWD,
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+except pymysql.err.OperationalError:
+        raise conf.MyException('ERROR- no connect to DB')
+
 
 from testapp import routes
